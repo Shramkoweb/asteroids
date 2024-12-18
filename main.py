abf803d429ecb38a4453ad915fea9ec420107024
Serhii Shramko <shramko.web@yahoo.com>
2024-12-18 15:28:17 -0500

    feat: add player initial render on game loop

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, TICK_TIME
+from player import Player
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
-            dt = clock.tick(TICK_TIME) / 1000
-            print(dt)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(TICK_TIME) / 1000
+
 
 if __name__ == "__main__":
     main()
