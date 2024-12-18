6addbb8d12d7b65c2af078a6d770dcddfa1aab9d
Serhii Shramko <shramko.web@yahoo.com>
2024-12-18 14:54:11 -0500

    refactor: add delta time calculation

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pygame
 import sys
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, TICK_TIME
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
     while True:
         for event in pygame.event.get():
+            dt = clock.tick(TICK_TIME) / 1000
+            print(dt)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
